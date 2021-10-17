005bb2b9e9e1980b983be711a1f4e6afaa36b25e
pastya <ghb.y14@gmail.com>
2021-10-17 13:13:39 +0300

    изменения в слайдере

--- js/script.js
+++ js/script.js
@@ -1,20 +1,3 @@
-// new Swiper('.slider-tab__news', {
-//   pagination: {
-//     el: '.swiper-pagination',
-//     clickable: true,
-//   },
-//         scrollbar: {
-//           el: '.swiper-scrollbar',
-//           draggable: true,
-//         },
-//         grabCursor: true,
-//         slidesPerView: 3.5,
-//         slidesPerGroup: 1,
-//         loop: true,
-// });
-
-
-
 $(document).ready(function() {
   $('.item-faq__title').click(function(event) {
     $(this).toggleClass('active').next().slideToggle(300);
@@ -25,9 +8,17 @@ var knowledgeSwiper = new Swiper('.knowledge__slider', {
   slidesPerView: 3.3,
   spaceBetween: 0,
   slidesPerGroup: 1,
+  
   pagination: {
-    el: ".swiper-pagination",
+    el: '.swiper-pagination',
     clickable: true,
   },
 });
+
+// $(document).ready(function() {
+//   $('.header__burger').click(function(event) {
+//       $('.header__burger,.header__menu').toggleClass('active');
+//       $('body').toggleClass('lock');
+//   });
+// });
 
